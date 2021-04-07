c4616e8a42491289a716ef933f4af9883455c12e
lijunjun <11776174@qq.com>
2021-04-07 22:19:39 +0800

    scss升级, 基本样式引入

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,13 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 关闭没有使用的变量警告
+    'no-unused-vars': 'off',
+    // 关闭对象的属性不能使用引号包裹
+    'quote-props': 'off',
+    // 可以使用反引号, 不强制使用单引号
+    'quotes': 'off'
+
   }
 }
---
+++ vue.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  devServer: {
+    // 自动开启浏览器
+    open: true,
+    // 一切服务都启用gzip 压缩：
+    compress: true,
+    // 热模块替换
+    hot: true,
+    port: 9100,
+    // 将运行进度输出到控制台。
+    progress: true
+  },
+  css: {
+    sourceMap: process.env.NODE_ENV === `development`,
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/assets/scss/variable.scss";
+          @import "@/assets/scss/mixin.scss";
+        `
+      }
+    }
+  }
+}
