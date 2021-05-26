dfc418c69da84fa7b266114b5b35335729139b12
lijunjun <11776174@qq.com>
2021-05-26 21:28:09 +0800

    test

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,15 +1,23 @@
 <template>
-  <div >
-    1
+  <div class="test">
+    1   32
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style scoped>
-
+<style
+  lang="scss"
+  scoped>
+.test {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: #fff;
+  color: #333;
+}
 </style>
